7ce2a556a764522bdf2c29c54e7ae49f17091bd2
asad-khilji <100327772+asad-khilji@users.noreply.github.com>
2025-08-01 11:15:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+fetch('products.json')
+  .then(res => res.json())
+  .then(data => {
+    const catalog = document.getElementById('catalog');
+
+    data.products.forEach(product => {
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      card.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <h3>${product.name}</h3>
+        <div class="size">Size: ${product.size}</div>
+        <div class="bottom">
+          <span>${product.code}</span>
+          <span>${product.price}</span>
+        </div>
+      `;
+
+      catalog.appendChild(card);
+    });
+  });
